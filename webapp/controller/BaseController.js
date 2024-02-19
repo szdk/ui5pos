d74ac276516fd18bf42aacc214ab08556d29ece4
sajid khan <askay005@gmail.com>
2024-02-19 19:49:28 +0000

    working on view_product edit feature

--- webapp/controller/BaseController.js
+++ webapp/controller/BaseController.js
@@ -1,10 +1,16 @@
 sap.ui.define([
         "sap/ui/core/mvc/Controller",
-        "sap/ui/core/routing/History"
+        "sap/ui/core/routing/History",
+        "sap/m/Dialog",
+        "sap/m/Button",
+        "sap/m/Text"
     ],
     function (
             Controller,
-            History
+            History,
+            Dialog,
+            Button,
+            Text
         ) {
         "use strict";
 
@@ -21,6 +27,64 @@ sap.ui.define([
                     window.history.go(-1);
                 else
                     this.comp.getRouter().navTo(defaultPage, parameters, true);
+            },
+
+            showErrorDialog: function (data) {
+                data = data || {};
+                data.title = data.title || this.i18n.getText('error');
+                data.buttonText = data.buttonText || this.i18n.getText('close');
+                data.message = data.message || [this.i18n.getText('an_error_occurred')];
+                data.onClose = data.onClose;
+
+                if (typeof data.message == 'string')
+                    data.message = [data.message];
+
+                //body
+                let messageContent = [];
+                for (let message of data.message) {
+                    if (typeof message == 'string')
+                        messageContent.push(new Text({ text: message }));
+                }
+                
+                //close button
+                let closeButton = new Button({
+                    type: sap.m.ButtonType.Emphasized,
+                    text: data.buttonText,
+                    press: () => {
+                        this._errorDialog.close();
+                        //destroy controlls
+                        this._errorDialog
+                            .destroyBeginButton()
+                            .destroyContent();
+                    }
+                });
+
+
+
+                if (!this._errorDialog) {
+                    this._errorDialog = new Dialog({
+                        type: sap.m.DialogType.Message,
+                        state: sap.ui.core.ValueState.Error,
+                        title: data.title,
+                        content: messageContent,
+                        beginButton: closeButton
+                    });
+                } else {
+                    this._errorDialog.setTitle(data.title);
+                    this._errorDialog.setBeginButton(closeButton);
+                    for (let content of messageContent)
+                        this._errorDialog.addContent(content);
+                }
+                
+                if (data.onClose) {
+                    let onClose = (evt) => {
+                        data.onClose(evt);
+                        this._errorDialog.detachAfterClose(onClose);
+                    }
+                    this._errorDialog.attachAfterClose(onClose);
+                }
+
+                this._errorDialog.open();
             }
             
         });
--- webapp/controller/product/View.controller.js
+++ webapp/controller/product/View.controller.js
@@ -1,39 +1,93 @@
 sap.ui.define([
     "ui5pos/szdk/controller/BaseController",
     "ui5pos/szdk/controller/util/RouteEvent",
+    "sap/ui/model/json/JSONModel"
     ],
-    function (Controller, RouteEvent) {
+    function (Controller, RouteEvent, JSONModel) {
         "use strict";
 
         return Controller.extend("ui5pos.szdk.controller.product.View", {
+            productID: null,
             onInit: function () {
                 Controller.prototype.onInit.apply(this, arguments);
                 
-                this.comp.getRouter().getRoute('view_product').attachPatternMatched((evt) => {
-                    if (!RouteEvent.defaultPatternMatched.apply(this, [evt])) return;
+                //========================= MODEL ============================================
+                this.mainModel = new JSONModel({
+                    editting : false,
+                    productData : {}
+                });
+                this.getView().setModel(this.mainModel, 'model');
 
-                    let arg = evt.getParameter('arguments');
-                    let id = arg && arg.id && ("" + arg.id).length > 0 ? arg.id : null;
+                this.comp.getRouter().getRoute('view_product').attachPatternMatched(this.patternMatched.bind(this));
 
-                    const notFound = () => {
-                        this.comp.getRouter().getTarget('notFound').display();
+            },
+
+            //============================================
+            onToggleEdit: function (evt) {
+                if (!this.mainModel.getProperty('/editting')) {
+                    if (!this.productID) return;
+                    
+                    const enableEdit = (data) => {
+                        if (!data || parseInt(!data.ProductID) !== parseInt(this.productID)) {
+                            this.showErrorDialog();
+                            return;
+                        }
+
+                        this.byId('product_view_page').setHeaderExpanded(true);
+                        this.mainModel.setProperty('/editting', true);
+
+                        //todo
+                        this.mainModel.setProperty('/productData', data);
                     };
 
-                    if (id) {
-                        this.comp.getModel('service').createBindingContext(`/Products(${id})`, {
-                            expand : 'Category,Order_Details,Supplier'
-                        },
-                        (context) => {
-                            if (!context) {
-                                notFound();
-                                return;
+
+                    let data = false && this.comp.getModel('service').getObject('', this.getView().getBindingContext('service'));
+                    if (!data || parseInt(!data.ProductID) !== parseInt(this.productID))
+                        this.comp.getModel('service').read(`/Products(${this.productID})`, {
+                            success : enableEdit,
+                            error : (err) => {
+                                this.showErrorDialog({message: err.message});
                             }
-                            this.getView().setBindingContext(context, 'service');
-                        }, true);
-                    } else {
-                        notFound();
-                    }
-                });
+                        });
+                    else
+                        enableEdit(data);                    
+
+                } else {
+                    this.mainModel.setProperty('/editting', false);
+                }
+            },
+
+
+            //============================================
+            patternMatched: function (evt) {
+                if (!RouteEvent.defaultPatternMatched.apply(this, [evt])) return;
+                this.getView().getModel('nav').setProperty('/sideNav/selectedKey', `nav_item_route_products`);
+
+                this.mainModel.setProperty('/editting', false);
+
+                let arg = evt.getParameter('arguments');
+                let id = arg && arg.id && ("" + arg.id).length > 0 ? arg.id : null;
+
+                const notFound = () => {
+                    this.comp.getRouter().getTarget('notFound').display();
+                };
+
+                if (id) {
+                    this.comp.getModel('service').createBindingContext(`/Products(${id})`, {
+                        expand : 'Category,Supplier'
+                    },
+                    (context) => {
+                        if (!context) {
+                            notFound();
+                            return;
+                        }
+                        //product found
+                        this.productID = id;
+                        this.getView().setBindingContext(context, 'service');
+                    }, true);
+                } else {
+                    notFound();
+                }
             }
         });
     }
