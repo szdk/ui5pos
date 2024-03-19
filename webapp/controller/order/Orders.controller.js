ff41fe3de61635ac3137c8f26defe6669eb11fbb
sajid khan <askay005@gmail.com>
2024-03-19 13:04:43 +0000

    working on orders page

--- webapp/controller/order/Orders.controller.js
+++ webapp/controller/order/Orders.controller.js
@@ -1,8 +1,15 @@
 sap.ui.define([
     "ui5pos/szdk/controller/BaseController",
     "ui5pos/szdk/controller/util/Table",
+    "sap/m/ObjectNumber",
+    "sap/m/Text",
     ],
-    function (Controller, myTable) {
+    function (
+        Controller,
+        TableGenerator,
+        ObjectNumber,
+        Text
+    ) {
         "use strict";
 
         return Controller.extend("ui5pos.szdk.controller.order.Orders", {
@@ -12,82 +19,76 @@ sap.ui.define([
                 window.orders = this;
 
                 this.comp.szdk_serviceCreated.then((model) => {
-                    this.tableGenerator = new myTable({
-                        i18n : this.i18n,
-                        model,
-                        id : this.getView().createId('orders_main_table'),
-                        properties : {
-                            growing : true,
-                            mode : sap.m.ListMode.SingleSelectMaster,
+                    let columns = [
+                        {id : 'OrderID', label : this.i18n.getText('order_id'), path : 'OrderID', header: {/*minScreenWidth:"XXLarge", width:"5em"*/}},
+                        {id : 'Customer', label : this.i18n.getText('customer'), path : 'Customer/ContactName', header: {minScreenWidth:"XXLarge", demandPopin:true, popinDisplay:"Inline"}},
+                        {id : 'OrderDate', label : this.i18n.getText('order_date'), path : 'OrderDate', header: {minScreenWidth:"XXLarge", demandPopin:true, popinDisplay:"Inline"}},
+                        {id : 'OrderTime', label : this.i18n.getText('order_time'), path : 'OrderDate', header: {minScreenWidth:"XXLarge", demandPopin:true, popinDisplay:"Inline"}},
+                        {id : 'Freight', label : this.i18n.getText('order_total'), path : 'Freight', header: {hAlign : "End"}},
+                    ].map((v) => ({
+                        id : this.getView().createId(v.id),
+                        cell: {bindingPath : v.path},
+                        header : {header : v.label, ...v.header},
+                        p13n : {key : v.id, label: v.label, path : v.path},
+                        excel : {label : v.label, property : v.path}
+                    }));
+                    columns[2].cell.control = new Text({text: {
+                        path : 'OrderDate',
+                        type : 'sap.ui.model.odata.type.Date',
+                    }});
+                    columns[2].p13n.label = this.i18n.getText('order_date_time');
+                    columns[3].cell.control = new Text({text: {
+                        path : 'OrderDate',
+                        type : {
+                            formatValue : (v) => (new Date(v).toTimeString().split(' ')[0])
                         },
-                        itemsBinding : {
-                            path : '/Orders'
-                        },
-                        toolbar : {
-                            p13n : {
-                                Columns : true,
-                                Sorter : true,
-                                Groups : true,
-                            },
-                            excel : 2,
-                            growSize : true,
-                            pin : true,
-                        },
-                        columns : [
-                            ['OrderID', 'Order ID'],
-                            ['OrderDate', 'Order Date'],
-                            ['Freight', 'Total'],
-                            ['CustomerID', 'Customer ID'],
-                        ].map((val) => {
-                            return {
-                                id : this.getView().createId(val[0]),
-                                p13n : {key : val[0], label : val[1], path : val[0]},
-                                cell : {bindingPath : val[0]},
-                                header : {header : val[1]},
-                                excel : {label : val[1], property : val[2] || val[0]}
-                            };
-                        }),
-                    });
-                    
-                    this.tableGenerator2 = new myTable({
-                        i18n : this.i18n,
-                        model,
-                        id : this.getView().createId('orders_main_table2'),
-                        properties : {
-                            mode : sap.m.ListMode.SingleSelectMaster,
-                            sticky : ['HeaderToolbar']
-                        },
-                        itemsBinding : {
-                            path : '/Products',
-                            parameters : {expand : 'Category'}
-                        },
-                        toolbar : {
-                            p13n : {
-                                Columns : true,
-                                Sorter : true,
-                                Groups : true,
-                            },
-                            excel : 2,
-                            growSize : true,
-                            pin : true,
-                        },
-                        columns : [
-                            ['ProductID', 'Product ID'],
-                            ['CategoryName', 'Category', 'Category/CategoryName'],
-                            ['UnitPrice', 'Price'],
-                        ].map((val) => {
-                            return {
-                                id : this.getView().createId(val[0] + '2'),
-                                p13n : {key : val[0] + '2', label : val[1], path : val[2] || val[0]},
-                                cell : {bindingPath : val[2] || val[0]},
-                                header : {header : val[1]},
-                                excel : {label : val[1], property : val[2] || val[0]}
-                            };
-                        }),
+                    }});
+                    delete columns[3].p13n;
+                    columns[4].cell.control = new ObjectNumber({unit : "USD"});
+                    columns[4].cell.control.bindProperty('number', {
+                        parts:[{path:'Freight'},{value:'USD'}],
+                        type: 'sap.ui.model.type.Currency',
+                        formatOptions: {showMeasure:false}
                     });
 
-                    this.byId('temp1').addItem(this.tableGenerator.getTable());
-                    this.byId('temp2').addItem(this.tableGenerator2.getTable());
+                    let tableGenerator = new TableGenerator({
+                        id: this.getView().createId('orders_table'),
+                        i18n: this.i18n,
+                        model,
+                        // columnListItem : {type : sap.m.ListType.Navigation},
+                        properties : {
+                            busyIndicatorDelay : 0,
+                            mode : sap.m.ListMode.SingleSelectMaster,
+                            rememberSelections:true,
+                            growing:true,
+                            growingThreshold:50,
+                            sticky: ['HeaderToolbar','ColumnHeaders'],
+                            popinLayout: sap.m.PopinLayout.Block,
+                            alternateRowColors: false,
+                        },
+                        itemsBinding : {
+                            path : '/Orders',
+                            parameters : {
+                                expand : 'Customer'
+                            }
+                        },
+                        customToolbar : {
+                            // start : [filterButton, new ToolbarSeparator(), inputOpenProduct, buttonOpenProduct]
+                        },
+                        toolbar : {
+                            p13n : {
+                                Columns : true,
+                                Sorter : true,
+                                Groups : true,
+                            },
+                            excel : this.comp.getModel('settings').getProperty('/odata/useMock') ? 2 : 1,
+                            growSize : true,
+                            pin : true
+                        },
+                        columns,
+                    });
+                    this.byId('table_container').addContent(tableGenerator.getTable());
+                    // tableGenerator.getTable().attachItemPress(this.onSelectionChange.bind(this)); todo
                 });
                 
             }
