4f5d55340ea11c87a65f62fed3eecfb22e7116c2
sajid khan <askay005@gmail.com>
2024-03-26 15:41:13 +0000

    create create_order layout

--- webapp/controller/order/CreateOrder.controller.js
+++ webapp/controller/order/CreateOrder.controller.js
@@ -20,7 +20,8 @@ sap.ui.define([
                         mode : 'input',
                         lock_quantity : false,
                         lock_discount : false,
-                    }
+                    },
+                    auto_print : false,
                 });
                 this.getView().setModel(this.configModel, 'config');
 
@@ -30,12 +31,29 @@ sap.ui.define([
                         quantity : 1,
                         discount : 0,
                         discount_type : 'usd',
-                    }
+                    },
+                    previous_orders : [
+                        {order_id : 10003, phone : '9876543210', order_total : 30.27},
+                        {order_id : 10002, phone : '9876543210', order_total : 54.00},
+                        {order_id : 10001, phone : '9876543210', order_total : 99.99}
+                    ],
                 });
                 this.getView().setModel(this.dataModel, 'data');
 
                 this.orderModel = new JSONModel({
                     total : 0,
+                    total_price : 0,
+                    total_discount : 0,
+                    items : [
+                        {name : 'Gumbo Mix', price : 14.99, discount : 0, quantity : 4, total: 59.96, editted : false},
+                        {name : 'Chiuawa', price : 9.00, discount : 0.15, quantity : 9, total: 12.15, editted : false}
+                    ],
+                    customer_phone : '',
+                    customer_name : '',
+                    customer_address : '',
+                    customer_city : '',
+                    customer_postal_code: '',
+                    customer_country : '',
                 });
                 this.getView().setModel(this.orderModel, 'order');
 
